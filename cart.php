80ac2f6628f3ec13b419fd20150ae0a8afbb6c23
Pascal <pascal.venema@windesheim.nl>
2020-11-10 11:17:18 +0100

    Begin table winkelwagen (zonder div)

--- cart.php
+++ cart.php
@@ -1,9 +1,24 @@
 <?php
 include __DIR__ . "/header.php";
+// Opzet winkelwage word als volgt:
+// Ilias maakt een array in de session met de volgende opzet:
+// $winkelwagen = array(42 => 1, 33 => 2)
+// De key is het productnummer en de value is het aantal.
 
+//TIJDELIJKE (TEST) ARRAY!!!:
+$_SESSION["cart"] = array(42=>1,233=>2);
 
-if(isset($_SESSION["cart"])){
+if(isset($_SESSION["cart"])) {
+    print '<table><tr>
+           <th>Verwijder product</th>
+           <th>Productnaam</th>
+           <th>Aantal</th>
+           <th>Prijs</th>
+           </tr>';
 
+    foreach ($_SESSION["cart"] as $productnummer => $aantal) {
+        print '<tr><th><input type="button"></th></tr>';
+    }
 }
 else{
     print 'Er zit niks in de winkelmand!';
