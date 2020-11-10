d175be78303a79078943a79a124e0bc1f1f8d5a7
bartdeboer18 <bartdeboer2014@gmail.com>
2020-11-10 11:13:11 +0100

    header

--- header.php
+++ header.php
@@ -22,19 +22,19 @@ include "connect.php";
     <link rel="stylesheet" href="Public/CSS/Style.css" type="text/css">
     <link rel="stylesheet" href="Public/CSS/bootstrap.min.css" type="text/css">
     <link rel="stylesheet" href="Public/CSS/nha3fuq.css">
-    <link rel="apple-touch-icon" sizes="57x57" href="Public/Favicon/apple-icon-57x57.png">
-    <link rel="apple-touch-icon" sizes="60x60" href="Public/Favicon/apple-icon-60x60.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="Public/Favicon/apple-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="76x76" href="Public/Favicon/apple-icon-76x76.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="Public/Favicon/apple-icon-114x114.png">
-    <link rel="apple-touch-icon" sizes="120x120" href="Public/Favicon/apple-icon-120x120.png">
-    <link rel="apple-touch-icon" sizes="144x144" href="Public/Favicon/apple-icon-144x144.png">
-    <link rel="apple-touch-icon" sizes="152x152" href="Public/Favicon/apple-icon-152x152.png">
-    <link rel="apple-touch-icon" sizes="180x180" href="Public/Favicon/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192" href="Public/Favicon/android-icon-192x192.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="Public/Favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="96x96" href="Public/Favicon/favicon-96x96.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="Public/Favicon/favicon-16x16.png">
+    <link rel="apple-touch-icon" sizes="57x57" href="Public/img/LOGONG.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="Public/img/LOGONG.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="Public/img/LOGONG.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="Public/img/LOGONG.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="Public/img/LOGONG.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="Public/img/LOGONG.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="Public/img/LOGONG.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="Public/img/LOGONG.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="Public/img/LOGONG.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="Public/img/LOGONG.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="Public/img/LOGONG.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="Public/img/LOGONG.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="Public/img/LOGONG.png">
     <link rel="manifest" href="Public/Favicon/manifest.json">
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="Public/Favicon/ms-icon-144x144.png">
@@ -65,19 +65,24 @@ include "connect.php";
                     ?>
                     <li>
                         <a href="browse.php?category_id=<?php print $HeaderStockGroup['StockGroupID']; ?>"
-                           class="HrefDecoration"><?php print $HeaderStockGroup['StockGroupName']; ?></a>
+                           class="HrefDecoration" style="font-size:20px"><?php print $HeaderStockGroup['StockGroupName']; ?></a>
                     </li>
                     <?php
                 }
                 ?>
                 <li>
-                    <a href="categories.php" class="HrefDecoration">Alle categorieën</a>
+                    <a href="categories.php" class="HrefDecoration" style="font-size:20px">  Alle categorieën</a>
+
                 </li>
             </ul>
         </div>
         <ul id="ul-class-navigation">
             <li>
-                <a href="browse.php" class="HrefDecoration"><i class="fas fa-search" style="color:#676EFF;"></i> Zoeken</a>
+                <a href="browse.php" class="HrefDecoration" ></a><i class="fas fa-search" style="color:#676EFF;" > </i> Zoeken</a>
+            </li>
+            &nbsp; &nbsp;
+            <li>
+                <a href="cart.php" class="HrefDecoration"><i class="fas fa-shopping-cart" style="color:#676EFF;"></i> Winkelmandje</a>
             </li>
         </ul>
     </div>
