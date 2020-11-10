3f72896cc505f1bd8dcfff1fd899605cd2a0b5fb
bartdeboer18 <bartdeboer2014@gmail.com>
2020-11-10 10:44:19 +0100

    logo

--- header.php
+++ header.php
@@ -43,9 +43,9 @@ include "connect.php";
 <body>
 <div class="Background">
     <div class="row" id="Header">
-        <div class="col-2"><a href="./" id="LogoA">
-                <div id="LogoImage"></div>
-            </a></div>
+        <div class="col-2"> <img src="Public/img/LOGONG.png">
+
+        </div>
         <div class="col-8" id="CategoriesBar">
             <ul id="ul-class">
                 <?php
