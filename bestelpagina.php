63b90ac46e3035793d11cfc411215dcde0136cae
sammyjwz <s1158612@student.windesheim.nl>
2020-11-12 14:48:38 +0100

    test

--- bestelpagina.php
+++ bestelpagina.php
@@ -7,15 +7,15 @@ include __DIR__ . "/header.php";
        Bestelpagina
     </h1>
 </header>
-<form>
-    Voornaam: <input type="text" required><br><br>
-    Tussenvoegsel: <input type="text"><br><br>
-    Achternaam: <input type="text" required><br><br>
-    Straatnaam: <input type="text" required><br><br>
-    Huisnummer: <input type="number" required><br><br>
-    Toevoeging: <input type="text"><br><br>
-    Woonplaats: <input type="text" required><br><br>
-    Telefoonnummer: <input type="text" required><br><br>
+<form method="get" action="overzicht-bestelling.php">
+    Voornaam: <input type="text" name="voornaam" required><br><br>
+    Tussenvoegsel: <input type="text" name="tussenvoegsel" ><br><br>
+    Achternaam: <input type="text" name="achternaam" required><br><br>
+    Straatnaam: <input type="text" name="straatnaam" required><br><br>
+    Huisnummer: <input type="number" name="huisnummer" required><br><br>
+    Toevoeging: <input type="text" name="toevoeging" maxlength="3" ><br><br>
+    Woonplaats: <input type="text" name="woonplaats" required><br><br>
+    Telefoonnummer: <input type="text" name="telefoonnummer" maxlength="10" required><br><br>
     <input type="submit" name="submit" value="Opslaan en naar de volgende stap">
 </form>
 <?php
