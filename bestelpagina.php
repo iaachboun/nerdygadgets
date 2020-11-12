a0c63ad275c8d7ef4d8390af464fec5ba0c65486
Arriparie <74239308+Arriparie@users.noreply.github.com>
2020-11-12 11:48:28 +0100

    Update bestelpagina.php

--- bestelpagina.php
+++ bestelpagina.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
-
+<?php
+include __DIR__ . "/header.php";
+?>
 <header>
     <h1>
        Bestelpagina
@@ -10,8 +12,17 @@
     Tussenvoegsel: <input type="text"><br><br>
     Achternaam: <input type="text" required><br><br>
     Straatnaam: <input type="text" required><br><br>
-    Huisnummer: <input type="number" required> Toevoeging: <input type="text"><br><br>
+    Huisnummer: <input type="number" required><br><br>
+    Toevoeging: <input type="text"><br><br>
     Woonplaats: <input type="text" required><br><br>
     Telefoonnummer: <input type="text" required><br><br>
-    <input type="submit">
+    <input type="submit" name="submit" value="Opslaan en naar de volgende stap">
 </form>
+<?php
+include __DIR__ . "/footer.php";
+?>
+<?php
+if(isset($_))
+
+
+?>
