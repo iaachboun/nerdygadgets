95cbc9872bcff0ddae9405b53fb6c844816d9ea1
Arriparie <s1160405@student.windesheim.nl>
2020-11-16 16:07:26 +0100

    placeholders aangepast

--- bestelpagina.php
+++ bestelpagina.php
@@ -7,42 +7,42 @@ include __DIR__ . "/header.php";
         <form>
             <div class="form-group">
                 <label for="Voornaam">Voornaam:</label>
-                <input type="email" class="form-control" id="Voornaam" aria-describedby="emailHelp"
+                <input type="text" class="form-control" id="Voornaam" aria-describedby="emailHelp"
                        placeholder="Enter voornaam">
             </div>
             <div class="form-group">
                 <label for="Tussenvoegsel">Tussenvoegsel:</label>
-                <input type="email" class="form-control" id="Tussenvoegsel" aria-describedby="emailHelp"
+                <input type="text" class="form-control" id="Tussenvoegsel" aria-describedby="emailHelp"
                        placeholder="Enter tussenvoegsel">
             </div>
             <div class="form-group">
                 <label for="Achternaam">Achternaam:</label>
-                <input type="email" class="form-control" id="Achternaam" aria-describedby="emailHelp"
+                <input type="text" class="form-control" id="Achternaam" aria-describedby="emailHelp"
                        placeholder="Enter achternaam">
             </div>
             <div class="form-group">
                 <label for="Straatnaam">Straatnaam:</label>
-                <input type="email" class="form-control" id="Straatnaam" aria-describedby="emailHelp"
+                <input type="text" class="form-control" id="Straatnaam" aria-describedby="emailHelp"
                        placeholder="Enter straatnaam">
             </div>
             <div class="form-group">
                 <label for="Huisnummer">Huisnummer:</label>
-                <input type="email" class="form-control" id="Huisnummer" aria-describedby="emailHelp"
+                <input type="number" class="form-control" id="Huisnummer" aria-describedby="emailHelp"
                        placeholder="Enter huisnummer">
             </div>
             <div class="form-group">
                 <label for="Toevoeging">Toevoeging:</label>
-                <input type="email" class="form-control" id="Toevoeging" aria-describedby="emailHelp"
+                <input type="text" class="form-control" id="Toevoeging" aria-describedby="emailHelp"
                        placeholder="Enter toevoeging">
             </div>
             <div class="form-group">
                 <label for="Woonplaats">Woonplaats:</label>
-                <input type="email" class="form-control" id="Woonplaats" aria-describedby="emailHelp"
+                <input type="text" class="form-control" id="Woonplaats" aria-describedby="emailHelp"
                        placeholder="Enter woonplaats">
             </div>
             <div class="form-group">
                 <label for="Telefoonnummer">Telefoonnummer:</label>
-                <input type="email" class="form-control" id="Telefoonnummer" aria-describedby="emailHelp"
+                <input type="text" class="form-control" id="Telefoonnummer" aria-describedby="emailHelp"
                        placeholder="Enter telefoonnummer">
             </div>
 
