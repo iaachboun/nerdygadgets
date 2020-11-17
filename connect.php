45cae96d8907971b8e25bc322a2f6044dd193b85
ilias <ilias@abbatis.nl>
2020-11-17 08:58:57 +0100

    test git ignore

--- connect.php
+++ connect.php
@@ -1,4 +1,5 @@
 <?php
+// test git ignore
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); // Set MySQLi to throw exceptions
 try {
     $Connection = mysqli_connect("localhost", "ilias", "12345", "nerdygadgets");
@@ -11,5 +12,3 @@ if (!$DatabaseAvailable) {
     ?><h2>Website wordt op dit moment onderhouden.</h2><?php
     die();
 }
-
-//testtttt.ttt
