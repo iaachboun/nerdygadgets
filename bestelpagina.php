b0eb5957bfe447141f42e213a55fc66a16759ec8
Pascal <pascal.venema@windesheim.nl>
2020-11-18 09:43:17 +0100

    bestelpagina update post

--- bestelpagina.php
+++ bestelpagina.php
@@ -9,16 +9,22 @@ include __DIR__ . "/header.php";
     <input type="text" class="form-group id="voornaam" name="voornaam" required ><br>
     <label for="tussenvoegsel">Tussenvoegsel:</label><br>
     <input type="text" id="tussenvoegsel" name="tussenvoegsel"><br>
-    <label for="voornaam">Achternaam:</label><br>
+    <label for="achternaam">Achternaam:</label><br>
     <input type="text" id="achternaam" name="achternaam" required><br>
     <label for="straatnaam">Straatnaam:</label><br>
     <input type="text" id="straatnaam" name="straatnaam" required><br>
     <label for="postcode">Postcode:</label><br>
-    <input type="text" id="postcode" name="postcode" required><br>
+    <input type="text" id="postcode" name="postcode" required maxlength="7"><br>
     <label for="huisnummer">Huisnummer:</label><br>
-    <input type="text" id="huisnummer" name="huisnummer" required><br>
-    <label for="huisnummer">Huisnummer:</label><br>
-    <input type="text" id="huisnummer" name="huisnummer" required><br>
+    <input type="number" id="huisnummer" name="huisnummer" required><br>
+    <label for="toevoeging">Toevoeging:</label><br>
+    <input type="text" id="toevoeging" name="toevoeging"><br>
+    <label for="woonplaats">Woonplaats:</label><br>
+    <input type="text" id="woonplaats" name="woonplaats" required><br>
+    <label for="telefoonnummer">Telefoonnummer:</label><br>
+    <input type="number" id="telefoonnummer" name="telefoonnummer" required min="9" max="10"><br>
+    <label for="email">E-mailadres:</label><br>
+    <input type="email" id="email" name="email" required><br>
 
 <button> <input type="submit" name="opslaan" class="btn btn-primary"> Opslaan en naar de volgende stap</button>
 </form>
