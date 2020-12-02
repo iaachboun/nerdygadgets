ed3d40852a0cfe7d89e6a918e1a604b66da5552d
Ilias aachboun <ili.aachboun1@gmail.com>
2020-12-02 17:30:34 +0100

    Give error alert when trying to log in with wrong credentials

--- login.php
+++ login.php
@@ -41,34 +41,40 @@ if (isset($_POST['submit'])) {
     $R = mysqli_stmt_get_result($Statement);
     $R = mysqli_fetch_all($R, MYSQLI_ASSOC);
 
-    //Test om ingelogde mail op te slaan in session
-    $_SESSION["email_login"] = $_POST['email'];
+    if (isset($R[0]["customerID"])) {
+        //Test om ingelogde mail op te slaan in session
+        $_SESSION["email_login"] = $_POST['email'];
 
-    //Session variabelen uit de post van bestelpagina
-    $_SESSION["customerID"] = $R[0]["customerID"];
-    $_SESSION["voornaam"] = $R[0]["firstname"];
-    if (isset($R[0]["insertion"])) {
-        $_SESSION["tussenvoegsel"] = $R[0]["insertion"];
-    }
-    $_SESSION["achternaam"] = $R[0]["lastname"];
-    $_SESSION["telefoonnummer"] = $R[0]["phone"];
-    $_SESSION["postcode"] = $R[0]["postal_code"];
-    $_SESSION["huisnummer"] = $R[0]["housenumber"];
-    if (isset($R[0]["addition"])) {
-        $_SESSION["toevoeging"] = $R[0]["addition"];
-    }
-    $_SESSION["woonplaats"] = $R[0]["city"];
-    $_SESSION["straatnaam"] = $R[0]["streetname"];
-    $_SESSION["email"] = $R[0]["email"];
+        //Session variabelen uit de post van bestelpagina
+        $_SESSION["customerID"] = $R[0]["customerID"];
+        $_SESSION["voornaam"] = $R[0]["firstname"];
+        if (isset($R[0]["insertion"])) {
+            $_SESSION["tussenvoegsel"] = $R[0]["insertion"];
+        }
+        $_SESSION["achternaam"] = $R[0]["lastname"];
+        $_SESSION["telefoonnummer"] = $R[0]["phone"];
+        $_SESSION["postcode"] = $R[0]["postal_code"];
+        $_SESSION["huisnummer"] = $R[0]["housenumber"];
+        if (isset($R[0]["addition"])) {
+            $_SESSION["toevoeging"] = $R[0]["addition"];
+        }
+        $_SESSION["woonplaats"] = $R[0]["city"];
+        $_SESSION["straatnaam"] = $R[0]["streetname"];
+        $_SESSION["email"] = $R[0]["email"];
 
-    $_SESSION['login'] = TRUE;
-    $html = '<div class="container">
-                <div class="row">
-                    <div class="col-6" style="margin: 0 auto">
-                        <p class="inlogStatus">Je bent ingelogd <a href="index.php"><button class="btn btn-primary">Ga Terug</button></a></p>
+        $_SESSION['login'] = TRUE;
+        $html = '<div class="container">
+                    <div class="row">
+                        <div class="col-6" style="margin: 0 auto">
+                            <p class="inlogStatus">Je bent ingelogd <a href="index.php"><button class="btn btn-primary">Ga Terug</button></a></p>
+                        </div>
                     </div>
-                </div>
-            </div>';
+                </div>';
+    } else {
+        $html = '<div class="container"> <p>Verkeerde inlog gegevens</p>';
+        $html .= ' <a href="login.php"><button class="btn btn-primary">Ga terug</button></a></div>';
+    }
+
     echo $html;
 
 } else {
